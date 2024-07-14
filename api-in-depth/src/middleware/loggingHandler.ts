d04b7ef3e7c83b8e8add1ea84497e1010d4f98f0
yongwoo <110705019+wooleejaan@users.noreply.github.com>
2024-07-14 19:02:03 +0900

    Added Data validation with Typescript and Decorators (#4)

--- api-in-depth/src/middleware/loggingHandler.ts
+++ api-in-depth/src/middleware/loggingHandler.ts
@@ -1,10 +1,10 @@
 import { Request, Response, NextFunction } from 'express';
 
 export function loggingHandler(req: Request, res: Response, next: NextFunction) {
-    logging.info(`Incomming - METHOD: [${req.method}] - URL: [${req.url}] - IP: [${req.socket.remoteAddress}]`);
+    logging.log(`Incomming - METHOD: [${req.method}] - URL: [${req.url}] - IP: [${req.socket.remoteAddress}]`);
 
     res.on('finish', () => {
-        logging.info(`Result - METHOD: [${req.method}] - URL: [${req.url}] - IP: [${req.socket.remoteAddress}] - STATUS: [${res.statusCode}]`);
+        logging.log(`Result - METHOD: [${req.method}] - URL: [${req.url}] - IP: [${req.socket.remoteAddress}] - STATUS: [${res.statusCode}]`);
     });
 
     next();
